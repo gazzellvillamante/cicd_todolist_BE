98d98206179a7d8f7bf803793b61e656ddbb24b0
admin <gazzellvillamante@gmail.com>
2025-03-31 19:05:01 +1300

    added test file

--- todolist_app/tests.py
+++ todolist_app/tests.py
@@ -1,3 +1,78 @@
 from django.test import TestCase
+from urllib import request
 
-# Create your tests here.
+from django.shortcuts import render
+
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+
+from todolist_app.serializer import UserSerializer
+from .models import Todo
+from todolist_app.serializer import TodoSerializer
+
+
+# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(LoginView, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'user_id': token.user_id})
+
+class LogoutView(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=200)
+
+# Create Todo item
+class TodoCreateView(generics.CreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+# View Todo items (list and single view)
+class TodoListView(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+class TodoDetailView(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+# Update Todo item
+class TodoUpdateView(generics.UpdateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+# Delete Todo item
+class TodoDeleteView(generics.DestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
